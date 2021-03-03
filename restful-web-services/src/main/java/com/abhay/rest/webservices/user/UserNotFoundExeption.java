6d8ab67f0c4da729ddcde111eefff8f4105a0bac
abhay771 <79136046+abhay771@users.noreply.github.com>
2021-03-03 16:48:24 +0530

    New SpringBoot Project

---
+++ restful-web-services/src/main/java/com/abhay/rest/webservices/user/UserNotFoundExeption.java
@@ -0,0 +1,14 @@
+package com.abhay.rest.webservices.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundExeption extends RuntimeException {
+
+	public UserNotFoundExeption(String message) {
+		super(message);
+		
+	}
+
+}
